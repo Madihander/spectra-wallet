68f2759326bc3e8dc09625e52019b39df12b949c
Madihander <madihanih2004@gmail.com>
2025-10-24 16:29:20 +0500

    put the logic for generating the master seed and the encryption key in a separate file

--- src/main.rs
+++ src/main.rs
@@ -18,47 +18,11 @@ mod entropy;
 mod hash;
 mod image_loader;
 mod utils;
-
+mod wallet_seeds;
 
 fn print_type<T: ?Sized>(_: &T) {
     println!("{}", std::any::type_name::<T>());
 }
-
-#[derive(Debug, Clone)]
-pub struct WalletSeeds {
-    pub master_seed: [u8; 64],           // Main Seed for key generation
-    pub key_encryption_key: [u8; 64],    // The key for encrypting the private key
-}
-impl WalletSeeds {
-    pub fn new(master_seed: [u8; 64], key_encryption_key: [u8; 64]) -> Self {
-        Self { master_seed, key_encryption_key }
-    }
-}
-
-pub fn generate_wallet_seeds(
-    primary_image:&PathBuf,
-    salt_image: &PathBuf,
-    color: &str,
-) -> Result<WalletSeeds> {
-    // 1) load image -> raw RGB bytes
-    let pixels1 = image_loader::load_image(primary_image)?;
-    let pixels2 = image_loader::load_image(salt_image)?;
-    let color_salt = utils::hex_color_to_bytes(color);
-        
-    // 2) get entropy blob (raw pixels for MVP)
-    let primary_entropy = entropy::entropy_by_frequency(&pixels1, false, 16);
-    let salt_entropy = entropy::from_raw_pixels(&pixels2);
-        
-    // 3) initial hash (Blake2b-512)
-    let master_seed = hash::blake2b_hash(&[primary_entropy, salt_entropy.clone(), color_salt.clone()].concat());
-            
-    // 4) hash for encryption key
-    let key_encryption_key = hash::blake2b_hash(&[salt_entropy, color_salt].concat());    
-    
-    let walletseeds = WalletSeeds::new(master_seed, key_encryption_key);
-    Ok(WalletSeeds::new(master_seed, key_encryption_key))
-    }
-
 
 
 #[derive(Parser, Debug)]
@@ -84,7 +48,7 @@ struct Args {
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let wallet = generate_wallet_seeds(&args.image1, &args.image2, &args.color)?;
+    let wallet = wallet_seeds::generate_wallet_seeds(&args.image1, &args.image2, &args.color)?;
 
 
     // println!("{:?}", wallet.master_seed);
