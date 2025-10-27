d4f7c9619801720b0dd2398ab350c6a6daa7dc70
Madihander <madihanih2004@gmail.com>
2025-10-27 14:34:41 +0500

    Adding a modified function to receive photo data not as a file, but as an array function for future "connect" with react

--- src/key_derivation/image_loader.rs
+++ src/key_derivation/image_loader.rs
@@ -1,6 +1,6 @@
 use image::ImageReader;
 use std::path::Path;
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 
 pub fn load_image<P: AsRef<Path>>(path: P) -> Result<Vec<u8>>{
     let path_ref = path.as_ref();
@@ -11,5 +11,17 @@ pub fn load_image<P: AsRef<Path>>(path: P) -> Result<Vec<u8>>{
         .with_context(|| format!("Failed to decode image: {}", path_ref.display()))?;
 
     let rgb = img.to_rgb8();
+    Ok(rgb.into_raw())
+}
+
+pub fn process_image(image_data: &[u8]) -> Result<Vec<u8>> {
+    // Load the image from memory
+    let img = image::load_from_memory(image_data)
+        .map_err(|e|anyhow!("Failed to load image: {}", e))?;
+
+    // Convert the image to RGB8 format
+    let rgb = img.to_rgb8();
+
+    // Return the raw RGB data
     Ok(rgb.into_raw())
 }
